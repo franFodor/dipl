e320953a700722a6431f027872437476f62f1621
franFodor <fodorkafran8@gmail.com>
2025-10-08 09:34:08 +0200

    add dsp library

---
+++ esp_prototip/README.md
@@ -0,0 +1,4 @@
+# ESP Prototip
+
+Ista stvar kao Arduino samo u ESP-IDF-u. Našao sam [web serial plotter](https://web-serial-plotter.atomic14.com/)
+pa sam to koristio.
--- esp_prototip/main/esp_prototip.c
+++ esp_prototip/main/esp_prototip.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#include "driver/i2s_common.h"
-#include "esp_log.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/projdefs.h"
-#include "freertos/task.h"
-
-#include "driver/i2s_std.h"
-#include "driver/gpio.h"
 #include <stdint.h>
 #include <string.h>
 
+#include "driver/i2s_common.h"
+#include "driver/i2s_std.h"
+#include "driver/gpio.h"
+#include "dsps_fft2r.h"
+#include "dsps_wind_hann.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "esp_dsp.h"
+#include "sdkconfig.h"
+#include "math.h"
 
 #define BUFFER_SIZE 1024
 
 static const char* TAG = "ESP32";
-
 static i2s_chan_handle_t rx_handle;
+
+float y[BUFFER_SIZE * 2];
+float wind[BUFFER_SIZE];
 
 static void i2s_setup(void) {
   i2s_chan_config_t chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_AUTO, I2S_ROLE_MASTER);
@@ -56,29 +61,52 @@ static void i2s_read_task(void *args) {
 
   while(1) {
     if (i2s_channel_read(rx_handle, r_buf, BUFFER_SIZE, &r_bytes, 1000) == ESP_OK) {
-      ESP_LOGI(TAG, "Read task %d bytes", r_bytes);
+      ESP_LOGI(TAG, "Read task %zu bytes", r_bytes);
+      for (int i = 0; i < BUFFER_SIZE; i++) {
+        y[i * 2 + 0] = r_buf[i] * wind[i];
+        y[i * 2 + 1] = 0;
+      } 
     } else {
       ESP_LOGI(TAG, "Read task fail!");
     }
 
+    dsps_fft2r_fc32(y, BUFFER_SIZE);
+    dsps_bit_rev_fc32(y, BUFFER_SIZE);
+    dsps_cplx2reC_fc32(y, BUFFER_SIZE);
+
+    for (int i = 0 ; i < BUFFER_SIZE / 2 ; i++) {
+        y[i] = 10 * log10f((y[i * 2 + 0] * y[i * 2 + 0] + y[i * 2 + 1] * y[i * 2 + 1]) / BUFFER_SIZE);
+      }
+
+    // Show power spectrum in 64x10 window from -100 to 0 dB from 0..N/4 samples
+    ESP_LOGW(TAG, "Signal x1");
+    dsps_view(y, BUFFER_SIZE / 2, 64, 10,  -600, 400, '|');
+    /*
     int64_t sum = 0;
     for(int i = 0; i < r_bytes / sizeof(int32_t); i++) {
       sum += abs(r_buf[i]);
     }
 
     int32_t average_amplitude = sum / (r_bytes / sizeof(int32_t));
-    ESP_LOGI(TAG, "%d", average_amplitude);
+    ESP_LOGI(TAG, "%ld", average_amplitude);
+    // za serial plotter
     // printf("%d\n", average_amplitude);
-    
+    */
+
     vTaskDelay(pdMS_TO_TICKS(200));
   }
 
   free(r_buf);
-  //vTaskDelete(NULL);
+  vTaskDelete(NULL);
 }
 
 void app_main(void) {
+  // i2s
   i2s_setup();
+
+  // esp_dsp - fft
+  ESP_ERROR_CHECK(dsps_fft2r_init_fc32(NULL, CONFIG_DSP_MAX_FFT_SIZE));
+  dsps_wind_hann_f32(wind, BUFFER_SIZE);
 
   xTaskCreate(i2s_read_task, "i2s_read_task", 4096, NULL, 5, NULL);
 }
